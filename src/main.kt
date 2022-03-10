d4fb3f8c49ccc7b18c634f4182a158910006eab2
Odoyo-Queenter <achiengodoyoqueenter@gmail.com>
2022-03-10 08:59:06 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,24 @@
+fun main(){
+    var y = division(40,3)
+    println(y)
+  //  var x = "Queenter, 19"
+   // println(x)
+
+}
+fun division(a:Int,b:Int):Int{
+    var modulus = a % b
+    return modulus
+
+
+}fun name(name:String):String{
+   // var me = "Hi, my name is $"
+   // var whole = "and i am $ years old"
+   // var both = "Hi, my name is $ and i am $ years old"
+
+}
+fun quality(quality:String):{
+    //var = "Queenter"
+    //var =
+
+
+}
