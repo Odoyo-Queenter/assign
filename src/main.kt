18c8b070e73a5fb370e3668150454bf4a7d9d71c
Odoyo-Queenter <achiengodoyoqueenter@gmail.com>
2022-03-12 10:23:35 +0300

    first commit

--- src/main.kt
+++ src/main.kt
@@ -1,24 +1,63 @@
 fun main(){
+    case(arrayOf("harare","mumbai","dodooma","jakarta"))
     var y = division(40,3)
     println(y)
-  //  var x = "Queenter, 19"
-   // println(x)
+    var x = addition(arrayOf(1,2,3,4,5))
+    println(x)
+    var s = sumIntegers(arrayOf("Kenya",5,3.2,false,7,1.2))
+    println(s)
+    var q = sumup(arrayOf("merry", 1,2, "katana" ,false,4.5, "joy"))
+    println(q)
+    var r = digits(arrayOf(17,4,213,78,43,90,31,3,73,11,158,62))
+    println(r)
+
+}
+fun case(cities:Array<String>){
+    cities.forEach { cities ->
+        println(cities.capitalize())
+    }
+
 
 }
 fun division(a:Int,b:Int):Int{
-    var modulus = a % b
-    return modulus
-
-
-}fun name(name:String):String{
-   // var me = "Hi, my name is $"
-   // var whole = "and i am $ years old"
-   // var both = "Hi, my name is $ and i am $ years old"
-
+    var modolus = a % b
+    return modolus
 }
-fun quality(quality:String):{
-    //var = "Queenter"
-    //var =
-
-
+//write a function that takes in an array of integers and calculates their sum without using the inbuilt
+// .sum() function
+fun addition(numb:Array<Int>):Int {
+    var sum = 0
+    numb.forEach { num ->
+        sum += num
+    }
+    return sum
 }
+//create a function that takes in an array of mixed types and sums up the integers only
+fun sumIntegers(medly:Array<Any>):Int{
+    var sum = 0
+    medly.forEach { element ->
+        if (element is Int){
+            sum+=element
+        }
+    }
+    return sum
+}
+fun sumup(zealy:Array<Any>):String{
+    var sum = ""
+    zealy.forEach { ed ->
+        if (ed is String){
+             sum+=ed
+        }
+    }
+    return sum
+}
+fun digits(numbers:Array<Int>):Int{
+    var together = numbers[2] + numbers[5]
+    return together
+}
+
+
+
+
+
+
